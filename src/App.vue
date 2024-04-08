82fa6e213094c8910406897e875dc2797e5f0a06
nicoleabrand <nicoleabrand13@gmail.com>
2024-04-08 15:11:04 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,147 @@
 <script setup>
 import { reactive, ref } from 'vue'
 
-const titulo = ref("Meu VueJS!");
+const titulo = 'Edição de perfil';
 const mostrarResultado = ref(false)
 
-const produto = reactive({
+const info = reactive({
   nome: '',
-  preco: 0,
-  estoque: 0,
-  categorias: []
+  email: '',
+  senha: 0,
+  confisenha: 0,
+  datanascimento: 0,
+  endereco: '',
+  cidade: '',
+  estado: [],
+  hobbies: '',
+  linguagemprog: [],
+  biografia: ''
 })
 
-const categorias=[
-{
-  id:1,
-  nome:'JOIAS'
-},
-{
-  id:2,
-  nome:'ELETROLUX'
-}]
+const estados = [
+  {
+    sigla: 'AC',
+    nome: 'Acre'
+  },
+  {
+    sigla: 'AL',
+    nome: 'Alagoas'
+  },
+  {
+    sigla: 'AP',
+    nome: 'Amapá'
+  },
+  {
+    sigla: 'AM',
+    nome: 'Amazonas'
+  },
+  {
+    sigla: 'BA',
+    nome: 'Bahia'
+  },
+  {
+    sigla: 'CE',
+    nome: 'Ceará'
+  },
+  {
+    sigla: 'DF',
+    nome: 'Distrito Federal'
+  },
+  {
+    sigla: 'ES',
+    nome: 'Espírito Santo'
+  },
+  {
+    sigla: 'GO',
+    nome: 'Goiás'
+  },
+  {
+    sigla: 'MA',
+    nome: 'Maranhão'
+  },
+  {
+    sigla: 'MT',
+    nome: 'Mato Grosso'
+  },
+  {
+    sigla: 'MG',
+    nome: 'Minas Gerais'
+  },
+  {
+    sigla: 'PA',
+    nome: 'Pará'
+  },
+  {
+    sigla: 'PB',
+    nome: 'Paraíba'
+  },
+  {
+    sigla: 'PR',
+    nome: 'Paraná'
+  },
+  {
+    sigla: 'PE',
+    nome: 'Pernambuco'
+  },
+  {
+    sigla: 'PI',
+    nome: 'Piuaí'
+  },
+  {
+    sigla: 'RJ',
+    nome: 'Rio de Janeiro'
+  },
+  {
+    sigla: 'RN',
+    nome: 'Rio Grande do Norte'
+  },
+  {
+    sigla: 'RS',
+    nome: 'Rio Grande do Sul'
+  },
+  {
+    sigla: 'RO',
+    nome: 'Rondônia'
+  },
+  {
+    sigla: 'RR',
+    nome: 'Roraima'
+  },
+  {
+    sigla: 'SC',
+    nome: 'Santa Catarina'
+  },
+  {
+    sigla: 'SP',
+    nome: 'São Paulo'
+  },
+  {
+    sigla: 'SE',
+    nome: 'Sergipe'
+  },
+  {
+    sigla: 'TO',
+    nome: 'Tocantins'
+  },
+]
 
+const linguagensprog = [
+  {
+    Nome: 'Python'
+  },
+  {
+    Nome: 'JavaScript'
+  },
+]
 
-function formatarPreco(preco) {
+/*function formatarPreco(preco) {
   try {
     return `R$ ${preco.toFixed(2).replace(".", ",")}`
   }
   catch (e) {
     return `Invalido`
   }
-}
+}*/
 
 </script>
 
@@ -38,35 +150,69 @@ function formatarPreco(preco) {
   <div class="container">
     <div class="formulario">
       <h2>Formulário</h2>
-      <input type="text" v-model="titulo" />
       <hr />
       <div class="row">
         <label for="">Nome: </label>
-        <input type="text" v-model="produto.nome" required>
+        <input type="text" v-model="info.nome" required>
       </div>
       <div class="row">
-        <label for="">Preço: </label>
-        <input type="number" v-model.number="produto.preco">
+        <label for="">E-mail: </label>
+        <input type="text" v-model.number="info.email">
       </div>
       <div class="row">
-        <label for="">Estoque: </label>
-        <input type="number" v-model="produto.estoque">
+        <label for="">Senha: </label>
+        <input type="number" v-model="info.senha">
+      </div>
+      <div class="row">
+        <label for="">Confirmar Senha: </label>
+        <input type="number" v-model="info.confisenha">
+      </div>
+      <div class="row">
+        <label for="">Data de Nascimento: </label>
+        <input type="number" v-model="info.datanascimento">
+      </div>
+      <div class="row">
+        <label for="">Endereco: </label>
+        <input type="text" v-model="info.endereco">
+      </div>
+      <div class="row">
+        <label for="">Cidade: </label>
+        <input type="text" v-model="info.cidade">
+      </div>
+      <div class="row">
+        <label for="info.estado">Estado:</label>
+        <select v-model="info.estado">
+          <option v-for="estado in estados" :key="estado.sigla" :value="estado.nome">({{ estado.sigla }})
+            {{ estado.nome }} </option>
+        </select>
+      </div>
+      <div class="row">
+        <label for="">Hobbies: </label>
+        <input type="text" v-model="info.hobbies">
       </div>
       <fieldset>
-        <legend>Categorias:</legend>
-        <div v-for="categoria in categorias" :key="categoria.id"><input type="checkbox" v-model="produto.categorias" :value= categoria.id /> {{categoria.nome}}</div>
+        <legend>Linguagem de programação: </legend>
+        <input v-model="info.linguagemprog" v-for="linguagem in linguagensprog" :value="linguagensprog.nome" type="checkbox">{{ linguagem.nome }}>
       </fieldset>
+      <div class="row">
+        <label for="">Biografia: </label>
+        <input type="text" v-model="info.biografia">
+      </div>
       <button @click="mostrarResultado = !mostrarResultado">Mostrar resultado</button>
     </div>
     <Transition>
       <div v-if="mostrarResultado" class="resultado">
-        <h2>Resultado</h2>
-        <h3> Dados do produto</h3>
-        <p>Nome: {{ produto.nome }}</p>
-        <p>Preco: {{ formatarPreco(produto.preco) }}</p>
-        <p>Estoque: {{ produto.estoque }}</p>
-        <p>Categorias: {{ produto.categorias }}</p>
-        <p> {{ mostrarResultado }}</p>
+        <h3>Perfil</h3>
+        <p>Nome: {{ info.nome }}</p>
+        <p>E-mail: {{ info.email }}</p>
+        <p>Senha: {{ info.senha }}</p>
+        <p>Data de Nascimento: {{ info.datanascimento }}</p>
+        <p>Endereço: {{ info.endereco }}</p>
+        <p>Cidade: {{ info.cidade }}</p>
+        <p>Estado: {{ info.estado }}</p>
+        <p>Hobbies: {{ info.hobbies }}</p>
+        <p>Linguagens de programação: {{ info.linguagemprog }}</p>
+        <p>Biografia: {{ info.biografia }}</p>
       </div>
     </Transition>
   </div>
@@ -86,11 +232,7 @@ function formatarPreco(preco) {
   width: 48vw;
   border-radius: 30px;
   padding: 10px;
-}
-
-.formulario,
-.formulario h2 {
-  background-color: rgb(165, 250, 250);
+  background-color: rgba(253, 253, 253, 0.514);
 }
 
 .resultado,
@@ -98,11 +240,6 @@ function formatarPreco(preco) {
   background-color: aquamarine;
 }
 
-.row,
-p,
-h3 {
-  background-color: rgb(117, 180, 159)
-}
 
 .v-enter-active,
 .v-leave-active {
@@ -116,7 +253,8 @@ h3 {
 .v-enter-to {
   opacity: 1;
 }
-fieldset{
-padding: 30px;
+
+fieldset {
+  padding: 30px;
 }
 </style>
